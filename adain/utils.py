c97043be06981dbdf49fb5875ad341db51b1fe90
Petar Mimica <petarmi@ext.inditex.com>
2022-09-20 12:39:41 +0200

    Resize with pad

--- adain/utils.py
+++ adain/utils.py
@@ -2,7 +2,29 @@
 
 import numpy as np
 import cv2
+from typing import Tuple
 
+def resize_with_pad(image: np.array,
+                    new_shape: Tuple[int, int],
+                    padding_color: Tuple[int] = (255, 255, 255)) -> np.array:
+    """Maintains aspect ratio and resizes with padding.
+    Params:
+        image: Image to be resized.
+        new_shape: Expected (width, height) of new image.
+        padding_color: Tuple in BGR of padding color
+    Returns:
+        image: Resized image with padding
+    """
+    original_shape = (image.shape[1], image.shape[0])
+    ratio = float(max(new_shape))/max(original_shape)
+    new_size = tuple([int(x*ratio) for x in original_shape])
+    image = cv2.resize(image, new_size)
+    delta_w = new_shape[0] - new_size[0]
+    delta_h = new_shape[1] - new_size[1]
+    top, bottom = delta_h//2, delta_h-(delta_h//2)
+    left, right = delta_w//2, delta_w-(delta_w//2)
+    image = cv2.copyMakeBorder(image, top, bottom, left, right, cv2.BORDER_CONSTANT, value=padding_color)
+    return image
 
 def preprocess(image, img_size=(224,224)):
     """
@@ -10,6 +32,14 @@ def preprocess(image, img_size=(224,224)):
         image : rgb-ordered array
     """
     image = np.expand_dims(cv2.resize(image.astype(np.float32), img_size), axis=0)
+    return image
+
+def preprocess_with_pad(image, img_size=(224,224)):
+    """
+    # Args
+        image : rgb-ordered array
+    """
+    image = np.expand_dims(resize_with_pad(image.astype(np.float32), img_size), axis=0)
     return image
 
 
